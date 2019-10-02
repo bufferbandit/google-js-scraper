3592ab0f62a102734df48f431480f0b2d1304311
kernelland <kernelland@protonmail.ch>
2019-10-02 15:56:37 +0200

    Update google.js

--- google.js
+++ google.js
@@ -1,12 +1,9 @@
 var urls = Array.prototype.slice.call(document.getElementsByTagName("a"))
-
 for(const url of urls) {
-
         if(!url.href.includes("google")  &&
            !url.href.includes("blogger") &&
            !url.href.startsWith("https://www.youtube.com")
         ){
-                console.log(url.href)
+                setTimeout (console.log.bind (console, url.href))
         }
-
 }
