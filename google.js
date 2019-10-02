01136fa9025eaa0c6625c79373390b2d273892aa
kernelland <kernelland@protonmail.ch>
2019-10-02 15:52:19 +0200

    Create google.js

---
+++ google.js
@@ -0,0 +1,12 @@
+var urls = Array.prototype.slice.call(document.getElementsByTagName("a"))
+
+for(const url of urls) {
+
+        if(!url.href.includes("google")  &&
+           !url.href.includes("blogger") &&
+           !url.href.startsWith("https://www.youtube.com")
+        ){
+                console.log(url.href)
+        }
+
+}
